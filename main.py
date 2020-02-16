50a2425042b8730783b5df65ce0283847e52045a
Groverkss <groverkss@gmail.com>
2020-02-16 16:01:51 +0530

    Initialised repo

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+
+pygame.init()
+
+# Game window dimensions
+window_width = 1920
+window_height = 1080
+
+game_display = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption("")
