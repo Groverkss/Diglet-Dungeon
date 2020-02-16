17ca39c072e4bafce87599bbd3ec5a736291c769
Groverkss <groverkss@gmail.com>
2020-02-16 17:19:21 +0530

    Added utility.py

---
+++ utility.py
@@ -0,0 +1,6 @@
+import pygame
+
+def draw_thing(game_display, thing_x, thing_y, 
+                thing_width, thing_height, color):
+    pygame.draw.rect(game_display, color, 
+    [thing_x, thing_y, thing_width, thing_height])
